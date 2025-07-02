ac5e03e369430e75a91924995ca172d64e670e88
bernandotorrez <bernandotorrez4@gmail.com>
2025-07-02 21:40:09 +0700

    feat: SP3M Update

--- app/Filament/Resources/Sp3mResource/Pages/EditSp3m.php
+++ app/Filament/Resources/Sp3mResource/Pages/EditSp3m.php
@@ -3,12 +3,69 @@
 namespace App\Filament\Resources\Sp3mResource\Pages;
 
 use App\Filament\Resources\Sp3mResource;
+use App\Models\Alpal;
+use App\Models\Bekal;
+use App\Models\KantorSar;
+use App\Models\Sp3m;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditSp3m extends EditRecord
 {
     protected static string $resource = Sp3mResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Apply ucwords() to the 'bekal' field before saving
+        $data['qty'] = (int) preg_replace('/[^\d]/', '', $data['qty']);
+        $data['harga_satuan'] = (int) preg_replace('/[^\d]/', '', $data['harga_satuan']);
+        $data['jumlah_harga'] = (int) preg_replace('/[^\d]/', '', $data['jumlah_harga']);
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        // Redirect to the list page after creation
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function beforeSave(): void
+    {
+        // Get input values
+        $id = $this->data['pagu_id'] ?? null;
+        $kantorSarId = $this->data['kantor_sar_id'] ?? null;
+        $alpalId = $this->data['alpal_id'] ?? null;
+        $bekalId = $this->data['bekal_id'] ?? null;
+        $tahunAnggaran = $this->data['tahun_anggaran'] ?? null;
+
+        // Check if the same record exists
+        $exists = Sp3m::where('kantor_sar_id', $kantorSarId)
+            ->where('alpal_id', $alpalId)
+            ->where('bekal_id', $bekalId)
+            ->where('tahun_anggaran', $tahunAnggaran)
+            ->where('sp3m_id', '!=', $id)
+            ->exists();
+
+        if ($exists) {
+            // Show Filament error notification
+            $dataKantorSar = KantorSar::find($kantorSarId);
+            $dataAlpal = Alpal::find($alpalId);
+            $dataBekal = Bekal::find($bekalId);
+
+            $message = 'Kantor SAR "'.ucwords($dataKantorSar->kantor_sar).'", Alpal "'.ucwords($dataAlpal->alpal).'", Bekal "'.ucwords($dataBekal->bekal).'" dan Tahun Anggaran "'.ucwords($tahunAnggaran).'" sudah ada';
+
+            Notification::make()
+                ->title('Error!')
+                ->body($message)
+                ->danger()
+                ->send();
+
+            // Prevent form submission
+            $this->halt();
+        }
+    }
 
     protected function getHeaderActions(): array
     {
