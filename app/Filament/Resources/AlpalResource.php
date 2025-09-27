48b72aef6f0f90e3f84019f0e302c8f9ef58d77e
bernandotorrez <bernandotorrez4@gmail.com>
2025-09-27 11:20:32 +0700

    adj: Change Order menu Transaksi

--- app/Filament/Resources/AlpalResource.php
+++ app/Filament/Resources/AlpalResource.php
@@ -27,7 +27,7 @@ class AlpalResource extends Resource
 
     protected static ?string $navigationLabel = 'Alpal Penggunaan BBM';
 
-    protected static ?int $navigationSort = 8;
+    protected static ?int $navigationSort = 2;
 
     protected static ?string $slug = 'alpal';
 
