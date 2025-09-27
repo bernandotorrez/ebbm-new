29669a1cd69b026d5ae3a1a571a278f45d96b9a5
bernandotorrez <bernandotorrez4@gmail.com>
2025-09-27 11:24:41 +0700

    adj: Change Order menu Transaksi

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -27,7 +27,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationLabel = 'Kelola User';
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 90;
 
     protected static ?string $slug = 'kelola-user';
 
